31b18eb6854e3f0a22642ce98dba574855cec431
Jorge Rodríguez-Araújo <jorge.rodriguez@aimen.es>
2016-06-21 11:24:59 +0200

    Added calibration button to qt_measures.

--- mashes_measures/scripts/qt_measures.py
+++ mashes_measures/scripts/qt_measures.py
@@ -8,18 +8,27 @@ from python_qt_binding import loadUi
 from python_qt_binding import QtGui
 from python_qt_binding import QtCore
 
-
-path = rospkg.RosPack().get_path('mashes_measures')
+from mashes_tachyon.msg import MsgCalibrate
 
 
 class QtMeasures(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
+        path = rospkg.RosPack().get_path('mashes_measures')
         loadUi(os.path.join(path, 'resources', 'measures.ui'), self)
 
+        self.btnCalibrate.clicked.connect(self.btnCalibrateClicked)
         self.btnRecord.clicked.connect(self.btnRecordClicked)
 
+        self.pub_calibrate = rospy.Publisher(
+            '/tachyon/calibrate', MsgCalibrate, queue_size=10)
+        self.msg_calibrate = MsgCalibrate()
+
         self.recording = False
+
+    def btnCalibrateClicked(self):
+        self.msg_calibrate.calibrate = 1
+        self.pub_calibrate.publish(self.msg_calibrate)
 
     def btnRecordClicked(self):
         if self.recording:
